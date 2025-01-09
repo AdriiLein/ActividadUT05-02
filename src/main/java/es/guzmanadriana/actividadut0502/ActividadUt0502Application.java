a17c03b970d1d81dcc0fea8ecc2adcfa8219cfce
AdriiLein <158595430+AdriiLein@users.noreply.github.com>
2025-01-09 17:18:19 +0100

    Adding UT05-02 repo

---
+++ src/main/java/es/guzmanadriana/actividadut0502/ActividadUt0502Application.java
@@ -0,0 +1,13 @@
+package es.guzmanadriana.actividadut0502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadUt0502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActividadUt0502Application.class, args);
+    }
+
+}
---
+++ src/test/java/es/guzmanadriana/actividadut0502/ActividadUt0502ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.guzmanadriana.actividadut0502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadUt0502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
